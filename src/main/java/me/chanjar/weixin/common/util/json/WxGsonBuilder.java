93f910ec8fd1bd8cb0e92a20c8adfe0a2bf62422
王大飞 <2335801716@qq.com>
2019-10-23 22:06:45 +0800

    Initial commit

---
+++ src/main/java/me/chanjar/weixin/common/util/json/WxGsonBuilder.java
@@ -0,0 +1,30 @@
+package me.chanjar.weixin.common.util.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import me.chanjar.weixin.common.bean.WxAccessToken;
+import me.chanjar.weixin.common.bean.menu.WxMenu;
+import me.chanjar.weixin.common.error.WxError;
+import me.chanjar.weixin.common.bean.result.WxMediaUploadResult;
+
+/**
+ * .
+ * @author chanjarster
+ */
+public class WxGsonBuilder {
+
+  private static final GsonBuilder INSTANCE = new GsonBuilder();
+
+  static {
+    INSTANCE.disableHtmlEscaping();
+    INSTANCE.registerTypeAdapter(WxAccessToken.class, new WxAccessTokenAdapter());
+    INSTANCE.registerTypeAdapter(WxError.class, new WxErrorAdapter());
+    INSTANCE.registerTypeAdapter(WxMenu.class, new WxMenuGsonAdapter());
+    INSTANCE.registerTypeAdapter(WxMediaUploadResult.class, new WxMediaUploadResultAdapter());
+  }
+
+  public static Gson create() {
+    return INSTANCE.create();
+  }
+
+}
